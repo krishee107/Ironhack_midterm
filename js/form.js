f7c3701da70fc0794cd58886aad1d89113c0a525
Cristina <krishee107@gmail.com>
2022-10-05 11:45:19 +0200

    Validaciones js

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ const submit = document.querySelector('.contact-form .button');
 
 submit.addEventListener('click', (e) =>{
     e.preventDefault();
-    contact = {};
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
 
     //Recogemos los valores
     let nombre = document.querySelector('.full-name-input') ;
@@ -10,17 +10,36 @@ submit.addEventListener('click', (e) =>{
     let phone  = document.querySelector('.phone-input') ;
     let message= document.querySelector('.message-input');
 
-    //Los añadimos
-    contact.nombre = nombre.value;
-    contact.email = email.value ;
-    contact.phone = phone.value;
-    contact.message = message.value;
-    //los mostramos
-    console.log(contact)
+    //Validar form
+    //Si no ha llenado los campos obligatorios
+    if(nombre.value.trim() === "" || message.value.trim() ===""){
+        window.alert('Es necesario llenar el nombre y el mensaje');
+    //Si el email no es un email
+    }else if(email.value.trim() != "" && !re.exec(email.value))
+        window.alert('El email no es válido');
+    //Si el teléfono no es válido
+    else if( phone.value != "" && ((phone.value).length != 9 || isNaN(phone.value)) )
+        window.alert('Teléfono no válido');
+    //Si todo está bien
+    else{
+        contact = {};
 
-    //vaciamos los imputs
-    nombre.value = "";
-    email.value = "";
-    phone.value = "";
-    message.value = "";
+
+
+        //Los añadimos
+        contact.nombre = nombre.value;
+        contact.email = email.value ;
+        contact.phone = phone.value;
+        contact.message = message.value;
+        //los mostramos
+        console.log(contact)
+    
+        //vaciamos los imputs
+        nombre.value = "";
+        email.value = "";
+        phone.value = "";
+        message.value = "";
+    }
+
 })
+
